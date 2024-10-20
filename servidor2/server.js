8fc2566486bbfb33c4cfcadecc89d550638f5068
Mauricio Martinez <68654041+mauriciomartinezm@users.noreply.github.com>
2024-10-20 14:50:41 -0500

    Add files via upload

---
+++ servidor2/server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { enviarCorreo } = require("./emailSender");
+//const { sendMessage } = require("./smsSender");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Declarar las variables correctamente
+let sensorValues = "";
+let commands = "";
+
+// Ruta para recibir los datos (POST)
+app.post("/recibir_datos", async (req, res) => {
+  console.log("Cuerpo: ", req.body);
+  sensorValues = req.body.sensor_values; // Acceder a sensor_values
+  commands = req.body.commands; // Acceder a commands
+
+  console.log("Valores de los sensores:", sensorValues);
+  console.log("Comandos:", commands);
+  res.json({ message: "Datos recibidos correctamente" });
+
+  // Enviar mensaje si se detecta un comando específico
+  //if (commands[2] === "Encender tercero") {
+  //  // Envía un SMS de alerta
+  //  //sendMessage("+573205056994");
+  //  // Envía un correo de alerta
+  //  try {
+  //    const resultado = await enviarCorreo(
+  //      "mauriciomartinez0416@gmail.com",
+  //      "Nivel del agua crítico",
+  //      "hola"
+  //    );
+  //    res.json({ message: resultado });
+  //  } catch (error) {
+  //    res.status(500).json({ message: `Error enviando el correo: ${error}` });
+  //  }
+  //}
+});
+
+// Nueva ruta para servir los datos almacenados (GET)
+app.get("/recibir_datos", (req, res) => {
+  res.json({ sensores: sensorValues, comandos: commands }); // Enviar los datos almacenados como JSON
+});
+
+//test
+app.get("/enviar_correo", async (req, res) => {
+  try {
+    const resultado = await enviarCorreo(
+      "mauriciomartinez0416@gmail.com",
+      "Nivel del agua crítico",
+      "<h1>Nivel del agua crítico</h1><h2>alertatemprana.space</h2>"
+    );
+    res.json({ message: resultado });
+  } catch (error) {
+    res.status(500).json({ message: `Error enviando el correo: ${error}` });
+  }
+});
+
+const server = app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
